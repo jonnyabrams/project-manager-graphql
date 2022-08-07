e0e4a419f15c6b4e130b431e4442037d3229e130
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-07 09:39:00 +0100

    feat: create Header & bring into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1>Oi oi</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>Oi oi</h1>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
