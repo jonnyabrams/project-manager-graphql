cc9d07582f4006cf1a2aff2658fea6bfb2114f84
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-09 10:30:20 +0100

    feat: can delete project

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 import Spinner from "../components/Spinner";
 import { GET_PROJECT } from "../queries/projectQueries";
 import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
 
 const Project = () => {
   const { id } = useParams();
@@ -28,6 +29,8 @@ const Project = () => {
           <p className="lead">{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
+
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
     </>
